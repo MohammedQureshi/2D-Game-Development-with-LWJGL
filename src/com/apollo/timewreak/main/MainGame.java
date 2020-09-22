d4e7373425dd757b45f1901853e2cd7df0f44739
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2020-09-22 22:42:46 +0100

    Added a display package and also moved MainGame class.

--- src/com/apollo/timewreak/main/MainGame.java
+++ src/com/apollo/timewreak/main/MainGame.java
@@ -1,9 +1,8 @@
-package com.apollo.timewreak;
+package com.apollo.timewreak.main;
 
 public class MainGame {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-
 }
