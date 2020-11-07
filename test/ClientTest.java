fba49404e8cd010514231c4cf8727e91a0d2819c
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-11-07 13:35:25 +0000

    creating a junit test for the Client class, needs to be fully implemented

--- test/ClientTest.java
+++ test/ClientTest.java
@@ -1,4 +1,13 @@
-package client;
+import com.apollo.timewreak.networking.Client;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ClientTest {
+    static Client c;
+
+    // TODO: correctly implement the client and Server, currently using main to test the connections.
+    public static void main(String[] args) {
+        c = new Client();
+        c.connect("127.0.0.1", 5542);
+    }
 }
