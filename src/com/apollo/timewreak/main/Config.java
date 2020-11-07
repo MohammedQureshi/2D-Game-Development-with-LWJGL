9d7cb170dd92f039c02c112c5080e396ede91053
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2020-11-07 20:27:31 +0000

    Small updates

--- src/com/apollo/timewreak/main/Config.java
+++ src/com/apollo/timewreak/main/Config.java
@@ -2,13 +2,15 @@ package com.apollo.timewreak.main;
 
 import java.util.HashMap;
 
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+
 
 public class Config {
     public static final String GAME_NAME = "Project Apollo";
     public static int GAME_WIDTH = 1280;
     public static int GAME_HEIGHT = 720;
     public static boolean DISPLAY_FULLSCREEN = false;
-
+    public static long PRIMARY_DISPLAY = glfwGetPrimaryMonitor();
     public static double MAX_FPS = 65;
     public static int CAMERA_SPEED = 5;
     public class Server {
--- src/com/apollo/timewreak/networking/Server.java
+++ src/com/apollo/timewreak/networking/Server.java
@@ -1,7 +1,5 @@
 package com.apollo.timewreak.networking;
 
-import com.sun.javafx.util.Logging;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
