36c67c8e363951916dea2ad311d7c67705a11ebf
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-11-07 13:37:05 +0000

    creating a base Lobby which will be updated at a later date.

---
+++ src/com/apollo/timewreak/networking/Lobby.java
@@ -0,0 +1,35 @@
+package com.apollo.timewreak.networking;
+
+import com.apollo.timewreak.main.Config;
+
+/**
+ * TODO: still needs complete implementation. I.e. adding GameMaps[] and GameModes[] (if we're adding these)
+ * */
+public class Lobby {
+    private Server server;
+    private int maxPlayerCount = 0;
+//    private Map[] playableMaps; // TODO: create a Map type class.
+
+    public Lobby() {
+        /** getting the value from the Config and getting the host port. */
+        server = new Server(Config.Server.HOST_PORT);
+
+        server.start();
+    }
+
+    public int getMaxPlayerCount() {
+        return this.maxPlayerCount;
+    }
+
+    public void setMaxPlayerCount(int newMaxPlayerCount) {
+        this.maxPlayerCount = newMaxPlayerCount;
+    }
+
+
+}
+
+// what do we need in a lobby?
+// 1. playerCount
+// 2. GameMode / GameType
+// 3. GameMap
+// 4.
