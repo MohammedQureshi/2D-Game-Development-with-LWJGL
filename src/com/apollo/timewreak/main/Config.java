731e21dc5d9285d5deab26af4aabc1046479cace
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-11-08 20:45:32 +0000

    updated to remove the Serve Class added and moved to a more appropriate place. The Server class itself now contains a DefaultConfig class (this is bound to change as the project progresses)

--- src/com/apollo/timewreak/main/Config.java
+++ src/com/apollo/timewreak/main/Config.java
@@ -9,13 +9,8 @@ public class Config {
     public static final String GAME_NAME = "Project Apollo";
     public static int GAME_WIDTH = 1280;
     public static int GAME_HEIGHT = 720;
-    public static boolean DISPLAY_FULLSCREEN = false;
-    public static long PRIMARY_DISPLAY = glfwGetPrimaryMonitor();
     public static double MAX_FPS = 65;
     public static int CAMERA_SPEED = 5;
-    public class Server {
-        public static final int HOST_PORT = 5432;
-    }
     public static final int GAME_SCALE = 16;
 
 }
