66de23ad380b813fa09c8abb1e212c0c12e66dff
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-11-12 03:12:41 +0000

    changed params to be final as they are not edited at a later stage.

--- src/com/apollo/timewreak/engine/AnimationHandler.java
+++ src/com/apollo/timewreak/engine/AnimationHandler.java
@@ -11,26 +11,31 @@ public class AnimationHandler {
     private double currentTime;
     private double lastTime;
     private double fps;
-
-    public AnimationHandler(int amount, int fps, String filename) {
+    private String fileName;
+    public AnimationHandler(final int amount, final double fps, final String fileName) {
+        this.fileName = fileName;
         this.pointer = 0;
         this.elapsedTime = 0;
         this.currentTime = 0;
         this.lastTime = TimerHandler.getTime();
-        this.fps = 1.0/(double)fps;
+        this.fps = 1.0/fps;
 
+
+        // TODO: this method needs to be changed.
         //Loads all the different files that use the animation.
         this.frames = new TextureHandler[amount];
         for(int i = 0; i < amount; i++) {
-            this.frames[i] = new TextureHandler("Animation/" + filename + "_" + i + ".png");
+            this.frames[i] = new TextureHandler("Animation/" + fileName + "_" + i + ".png");
         }
     }
-    
+
+    public String getFileName() { return this.fileName; }
+
     //Bind the texture
     public void bind() { bind(0); }
 
     //Run though the multiple textures and binds it each time a new texture comes onto screen
-    public void bind(int sampler) {
+    public void bind(final int sampler) {
         this.currentTime = TimerHandler.getTime();
         this.elapsedTime += currentTime - lastTime;
 
